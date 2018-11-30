15689d07467b7e85c78cf504bcf11babe2829ba5
심철환 <a@akaiv.com>
2018-12-01 02:37:35 +0900

    기능: 베팅 상자를 클릭하면 베팅을 취소

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import classNames from 'classnames'
 import style from './Player.module.scss'
-import Box from './Box'
 
 const Player = ({ result, hand, bet, actions, isDealer, active }) => (
   <article className={classNames(style.player, active && style.active)}>
     {!isDealer && <section className={style.result}>{result}</section>}
-
     <section className={style.hand}>{hand}</section>
-
-    {!isDealer && (
-      <section className={style.bet}>
-        <Box>{bet}</Box>
-      </section>
-    )}
+    {!isDealer && <section className={style.bet}>{bet}</section>}
   </article>
 )
 
--- src/containers/Player.js
+++ src/containers/Player.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as actions from '../redux/actions'
 import { getCurrentPlayer } from '../rules/check'
 import getResult from '../rules/getResult'
 import Player from '../components/Player'
+import Box from '../components/Box'
 import Hand from './Hand'
 
-export default connect(({ players }, { playerKey }) => ({
-  result: getResult(players, playerKey),
-  hand: <Hand playerKey={playerKey} />,
-  bet: players[playerKey].stake,
-  isDealer: playerKey === 'dealer',
-  active:
-    playerKey === 'dealer' ||
-    getCurrentPlayer(players) === playerKey ||
-    !!players['replica'].status
-}))(Player)
+export default connect(
+  state => state,
+  dispatch => bindActionCreators(actions, dispatch),
+  ({ players, game }, { bet }, { playerKey }) => {
+    const { stake } = players[playerKey]
+    const isDealer = playerKey === 'dealer'
+    const onBoxClick = () => !game.isPlaying && bet(playerKey, -1 * stake)
+    return {
+      result: getResult(players, playerKey),
+      hand: <Hand playerKey={playerKey} />,
+      bet: <Box onClick={onBoxClick}>{stake}</Box>,
+      isDealer: isDealer,
+      active:
+        isDealer ||
+        getCurrentPlayer(players) === playerKey ||
+        !!players['replica'].status
+    }
+  }
+)(Player)
