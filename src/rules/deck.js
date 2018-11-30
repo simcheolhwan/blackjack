1c57b4e40ae51abfb632aff8504188805d590682
심철환 <a@akaiv.com>
2018-12-01 03:58:32 +0900

    테스트: 테스트 케이스 추가

--- src/rules/deck.js
+++ src/rules/deck.js
@@ -1,4 +1,4 @@
-const deck = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+const deck = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 const reducer = (accumulator, currentValue) => [
   ...accumulator,
   ...Array.from({ length: 4 }, _ => currentValue)
--- src/test.js
+++ src/test.js
@@ -3,16 +3,20 @@ const shuffledDeck = getShuffledDeck()
 
 const testCase = ''
 const testDeck = {
-  autoStay: ['9', '5', '7', '7'],
-  dealerBlackjack: ['9', '5', 'A', '7', 'K'],
-  dealerBust: ['K', 'K', 'K', 'K', 'Q'],
-  blackjack: ['A', 'K', '6'],
-  blackjackWin: ['A', 'K', 'A', '2'],
-  blackjackDraw: ['A', 'K', 'K', 'A'],
-  blackjackAfterSplit: ['A', 'A', 'A', 'K', 'K', '9'],
-  split: ['A', 'A'],
-  das: ['8', '8', '6', '3', '2']
+  hasAce: ['A'], // A로 시작
+  split: ['A', 'A'], // 스플릿으로 시작
+  das: [8, 8, 6, 3, 2], // [SP, D] 스플릿 후 더블
+  autoStay: [9, 5, 7, 7], // [H] 21이므로 자동으로 멈춰야 함
+  dealerBust: ['K', 'K', 'K', '6', 'Q'], // [S] 딜러가 버스트되면서 플레이어가 이겨야 함
+  dealerBlackjack: [9, 5, 'A', 7, 'K'], // [H] 서로 21이지만 딜러가 이겨야 함
+  blackjack: ['A', 'K', 6], // [] 블랙잭으로 즉시 이겨야 함
+  blackjackWin: ['A', 'K', 'A', 2], // [] 블랙잭이지만 딜러가 1장 뽑은 후 이겨야 함
+  blackjackDraw: ['A', 'K', 'K', 'A'], // [] 블랙잭이지만 비겨야 함
+  blackjackAfterSplit: ['A', 'A', 'A', 'K', 'K', 9], // [SP, H, H] 스플릿 후 모두 블랙잭
+  dealerBustAfterBlackjackAfterSplit: ['A', 'A', 'J', 9, 'A', 'Q', 3, 10] // [SP, H, H, H] 두 번째 플레이어가 블랙잭으로 이겨야 함
 }
 
 export default () =>
-  testCase ? { deck: [...testDeck[testCase], ...shuffledDeck] } : undefined
+  testDeck[testCase]
+    ? { deck: [...testDeck[testCase], ...shuffledDeck] }
+    : undefined
