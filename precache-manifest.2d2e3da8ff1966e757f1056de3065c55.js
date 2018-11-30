9c30cfb99b4f1aa32d7d30443771c4d9e356ec21
심철환 <a@akaiv.com>
2018-11-30 10:30:48 +0900

    Updates

--- precache-manifest.2d2e3da8ff1966e757f1056de3065c55.js
+++ precache-manifest.2d2e3da8ff1966e757f1056de3065c55.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "688a91f76760d90b0042",
+    "revision": "242a3e54de554d5ae95f",
     "url": "/blackjack/static/css/main.8c3f8adc.chunk.css"
   },
   {
-    "revision": "688a91f76760d90b0042",
-    "url": "/blackjack/static/js/main.688a91f7.chunk.js"
+    "revision": "242a3e54de554d5ae95f",
+    "url": "/blackjack/static/js/main.242a3e54.chunk.js"
   },
   {
     "revision": "6d6ca15121e036bc8e35",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/blackjack/static/js/runtime~main.4ac6e23b.js"
   },
   {
-    "revision": "388ec4c5876a37832ad0e7815d5aad43",
+    "revision": "e2f369096401f3840501d26cc13a445d",
     "url": "/blackjack/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/blackjack/precache-manifest.ab0490fccb7121f03b5e22fb06b42e72.js"
+  "/blackjack/precache-manifest.2d2e3da8ff1966e757f1056de3065c55.js"
 );
 
 workbox.clientsClaim();
