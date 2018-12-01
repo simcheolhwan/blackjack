b7e4e3f424d951cb08cfb2bd6267b20679c2987b
심철환 <a@akaiv.com>
2018-12-02 05:22:20 +0900

    개선: 최대 기록할 수 있는 게임 횟수 선언

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,4 @@ export const MIN = 5
 export const CAPITAL = 100
 export const BET = [5, 10, 25, 50, 100]
 export const DECK = 4
+export const HISTORY = 9999
--- src/redux/history.js
+++ src/redux/history.js
@@ -1,7 +1,9 @@
+import { HISTORY } from '../constants'
+
 export default (state = [], action) => {
   switch (action.type) {
     case 'record':
-      return [...state, action.payload]
+      return [...state.slice(-1 * HISTORY), action.payload]
 
     default:
       return state
