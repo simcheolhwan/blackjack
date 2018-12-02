615cda3e9f22d5d65308cb6bcd7984197a6b1465
심철환 <a@akaiv.com>
2018-12-02 20:10:16 +0900

    개선: 매우 큰 숫자가 박스 안에 있을 때 처리

--- src/components/Number.js
+++ src/components/Number.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import classNames from 'classnames'
 import style from './Number.module.scss'
 
 const Number = ({ children }) => {
@@ -6,7 +7,7 @@ const Number = ({ children }) => {
   const decimal = children - integer
 
   return (
-    <span className={style.number}>
+    <span className={classNames(style.number, integer > 9999 && style.large)}>
       <span>{String(integer)}</span>
       {integer < 1000 && !!decimal && <small>{String(decimal).slice(1)}</small>}
     </span>
