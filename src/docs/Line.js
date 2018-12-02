63c7d984fb348fc1b369296167c43926ad0bada2
심철환 <a@akaiv.com>
2018-12-02 13:51:50 +0900

    기능: Line 컴포넌트의 가로폭을 계산

--- src/docs/Line.js
+++ src/docs/Line.js
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, { Component, createRef } from 'react'
 import style from './Line.module.scss'
 import Number from '../components/Number'
 
-const Line = ({ index, chips, debt, max }) => (
-  <li className={style.line}>
-    <span className={style.index}>{index}</span>
-    <div className={style.track}>
-      <div className={style.bar} style={{ width: (100 * chips) / max + '%' }} />
-      <span className={style.number}>
-        <Number>{chips}</Number>
-      </span>
-    </div>
-    <span className={style.debt}>{!!debt && -1 * debt}</span>
-  </li>
-)
+const NUMBER = 50
+class Line extends Component {
+  track = createRef()
+  state = { width: 0 }
+
+  componentDidMount() {
+    const { width } = this.track.current.getBoundingClientRect()
+    this.setState({ width: width - NUMBER })
+  }
+
+  calcBarWidth = () => {
+    const { chips, max } = this.props
+    const { width } = this.state
+    return width * (chips / max)
+  }
+
+  render() {
+    const { index, chips, debt } = this.props
+
+    return (
+      <li className={style.line}>
+        <span className={style.index}>{index}</span>
+        <div className={style.track} ref={this.track}>
+          <div className={style.bar} style={{ width: this.calcBarWidth() }} />
+          <span className={style.number}>
+            <Number>{chips}</Number>
+          </span>
+        </div>
+        <span className={style.debt}>{!!debt && -1 * debt}</span>
+      </li>
+    )
+  }
+}
 
 export default Line
