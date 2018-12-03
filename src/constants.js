39755a3254662f17de5ac366c9379a11c4522c1d
심철환 <a@akaiv.com>
2018-12-03 14:40:10 +0900

    변경: 덱을 6개로 변경

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const UNIT = 5
 export const SEED = 100
 export const BET = [UNIT * 1, UNIT * 2, UNIT * 5, UNIT * 10, UNIT * 20]
-export const DECK = 4
+export const DECK = 6
 export const HISTORY = 9999
