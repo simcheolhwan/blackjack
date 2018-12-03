cc0472e71cc06203c3f116b00caf8349011ed251
심철환 <a@akaiv.com>
2018-12-03 14:41:01 +0900

    Updates

---
+++ precache-manifest.f73a123875fa43cfad7ace4b082ac62e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ac6e23b38f07c187510",
+    "url": "/blackjack/static/js/runtime~main.4ac6e23b.js"
+  },
+  {
+    "revision": "9ab10cff6972a4021aa0",
+    "url": "/blackjack/static/js/main.9ab10cff.chunk.js"
+  },
+  {
+    "revision": "37f2be777f794aafae9d",
+    "url": "/blackjack/static/js/1.37f2be77.chunk.js"
+  },
+  {
+    "revision": "9ab10cff6972a4021aa0",
+    "url": "/blackjack/static/css/main.54399a8f.chunk.css"
+  },
+  {
+    "revision": "00e50bf04faf6efc51f29779af38aaf2",
+    "url": "/blackjack/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/blackjack/precache-manifest.912fdc8b90f756ed5c3be11c97724810.js"
+  "/blackjack/precache-manifest.f73a123875fa43cfad7ace4b082ac62e.js"
 );
 
 workbox.clientsClaim();
