ecc1757d6cfa19faabaed3efc0090c63408b52a2
심철환 <a@akaiv.com>
2018-12-05 03:09:33 +0900

    변경: 기록 삭제 기능 제거

--- src/docs/Front.js
+++ src/docs/Front.js
@@ -1,19 +1,8 @@
-import React, { memo } from 'react'
-import classNames from 'classnames'
+import React from 'react'
 import style from './Front.module.scss'
-import RemoveState from '../containers/RemoveState'
 
 const Front = () => (
-  <RemoveState
-    render={({ increase, hasRemoved }) => (
-      <h1
-        className={classNames(style.title, hasRemoved && style.lineThrough)}
-        onClick={increase}
-      >
-        {process.env.REACT_APP_TITLE}
-      </h1>
-    )}
-  />
+  <h1 className={style.title}>{process.env.REACT_APP_TITLE}</h1>
 )
 
-export default memo(Front)
+export default Front
