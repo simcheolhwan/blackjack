be1632ba8ceace700687ccc61b24f8029a60203b
심철환 <a@akaiv.com>
2018-12-05 00:44:59 +0900

    리팩토링: 게임 기록 페이지에서 컴포넌트와 셀렉터를 추출

--- src/docs/History.js
+++ src/docs/History.js
@@ -1,20 +1,5 @@
-import React from 'react'
 import { connect } from 'react-redux'
-import style from './History.module.scss'
-import Chart from '../components/Chart'
-
-const History = ({ history, max, debt }) => (
-  <article className={style.article}>
-    <header className={style.header}>
-      {history.length} Games | Record {max} | Debt {debt}
-    </header>
-
-    <Chart max={max}>{history.slice(-1000).reverse()}</Chart>
-  </article>
-)
-
-export default connect(({ history, debt }) => {
-  const reduceMax = (acc, { chips, debt }) => [...acc, chips, debt]
-  const max = history.length ? Math.max(...history.reduce(reduceMax, [])) : 0
-  return { history, debt, max }
-})(History)
+import selector from '../selectors/history'
+import HistoryDetail from './HistoryDetail'
+const mapStateToProps = ({ history, debt }) => selector(1000, debt)(history)
+export default connect(mapStateToProps)(HistoryDetail)
---
+++ src/selectors/history.js
@@ -0,0 +1,11 @@
+export default (n, debt) => history => {
+  const reduceMax = (acc, { chips, debt }) => [...acc, chips, debt]
+  const { length } = history
+  const max = length ? Math.max(...history.reduce(reduceMax, [])) : 0
+  const summary = [length + ' Games', 'Best ' + max, debt && 'Dept ' + debt]
+  return {
+    summary: summary.filter(Boolean).join(' | '),
+    list: history.slice(-1 * n),
+    max
+  }
+}
