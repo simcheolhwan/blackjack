d8796fcb8941692ed4dc47d781b3c02575cc52a4
심철환 <a@akaiv.com>
2018-12-09 02:19:54 +0900

    기능: 자동 진행에 300ms 딜레이 부여

--- src/watch.js
+++ src/watch.js
@@ -2,7 +2,7 @@ import p from './rules/player'
 import d from './rules/dealer'
 import h from './rules/hand'
 
-export default (store, callback) => {
+export default ({ dispatch, getState }, callback) => {
   const watchPlayer = () => {
     const { can, must } = p({ player, bank }, turn)
     const { bust } = h(player[turn].hand)
@@ -19,11 +19,12 @@ export default (store, callback) => {
     return must.draw && { type: 'draw', card: deck[0] }
   }
 
-  const state = store.getState()
+  const state = getState()
   callback(state)
 
   const { deck, player, dealer, bank, turn } = state
   const next =
     Number.isInteger(turn) && (player[turn] ? watchPlayer() : watchDealer())
-  next && store.dispatch(next)
+
+  next && setTimeout(() => dispatch(next), 300)
 }
