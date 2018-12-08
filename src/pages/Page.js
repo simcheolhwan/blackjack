d340bc90597dbae521b91163e32ffb9d53e90fcf
심철환 <a@akaiv.com>
2018-12-09 02:42:42 +0900

    기능: 최대 베팅액을 설정하고 안내

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -4,7 +4,7 @@ import style from './Page.module.scss'
 const Page = ({ title, content }) => (
   <article>
     <h1 className={style.title}>{title}</h1>
-    <p>{content}</p>
+    <p className={style.content}>{content}</p>
   </article>
 )
 
