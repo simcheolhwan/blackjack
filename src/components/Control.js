4251d3903e93717e5e2de072c6d64196dbf53b09
심철환 <a@akaiv.com>
2018-12-09 15:21:09 +0900

    디자인: 컨트롤 영역

---
+++ src/components/Control.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './Control.module.scss'
+
+const Control = ({ onClick, children }) =>
+  onClick ? (
+    <button onClick={onClick} className={style.button}>
+      {children}
+    </button>
+  ) : null
+
+export default Control
--- src/containers/Controls.js
+++ src/containers/Controls.js
@@ -2,25 +2,22 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as actions from '../actions/game'
 import g from '../rules/game'
-import colors from '../styles/colors'
-import ActionGroup from '../components/ActionGroup'
+import Control from '../components/Control'
 
 export default connect(
   state => state,
   dispatch => bindActionCreators(actions, dispatch),
-  ({ player, dealer, turn }, { start, finish }) => ({
-    actions: [
+  ({ player, dealer, turn }, { start, finish }) =>
+    Object.assign(
+      {},
       Number.isInteger(turn)
         ? g({ player, dealer, turn }).hasFinished && {
-            color: colors['navy'],
             children: '✓',
             onClick: finish
           }
         : player[0].bets && {
-            color: colors['navy'],
             children: '→',
             onClick: start
           }
-    ].filter(Boolean)
-  })
-)(ActionGroup)
+    )
+)(Control)
