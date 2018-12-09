fc9f1a1f3e2582a810245ad621cd1f22c40b8c06
심철환 <a@akaiv.com>
2018-12-09 19:25:14 +0900

    디자인: 테이블 다듬음

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import style from './Table.module.scss'
 
-const Table = ({ dealer, player, bank, actions, controls }) => (
+const Table = ({ dealer, player = [], bank, actions, controls }) => (
   <main className={style.table}>
     <section className={style.dealer}>{dealer}</section>
-    <section className={style.player}>{player}</section>
+    {!!player.length && <section className={style.player}>{player}</section>}
     <section className={style.actions}>{actions}</section>
     <footer className={style.footer}>
       <section className={style.bank}>{bank}</section>
