bb9d6aefdb162a741a8cb39b2268b82aea7812ef
심철환 <a@akaiv.com>
2019-09-24 10:06:11 +0800

    Updates

---
+++ precache-manifest.084ea9e42746d949cb19ac093688fd1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c5efa905482fe759fbd34cdaf7dd00",
+    "url": "/blackjack/index.html"
+  },
+  {
+    "revision": "00a09042def634d0f8c9",
+    "url": "/blackjack/static/css/main.68ab6050.chunk.css"
+  },
+  {
+    "revision": "62692bbad88cb92582e3",
+    "url": "/blackjack/static/js/2.bd8b321f.chunk.js"
+  },
+  {
+    "revision": "00a09042def634d0f8c9",
+    "url": "/blackjack/static/js/main.379a214e.chunk.js"
+  },
+  {
+    "revision": "4a8faaca21edb796d84f",
+    "url": "/blackjack/static/js/runtime-main.d0b36f68.js"
+  }
+]);
